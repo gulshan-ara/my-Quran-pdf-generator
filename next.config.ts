51ae6725b9d0f9f9097ca59ad790ebbf5eb2cf7f
gulshan-ara <gulshan3015@gmail.com>
2025-07-02 11:11:17 +0600

    Added next config to fix the chromium package issue

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    // This helps with serverless function packaging
+    serverComponentsExternalPackages: ["@sparticuz/chromium"],
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
